a834575a94ea7961c1cc801d1746ed42eb336338
zeb32ra <zeb32ra@gmail.com>
2022-11-23 22:27:55 +0300

    Almost done

--- Main.cs
+++ Main.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,35 +10,59 @@ namespace Wire
 {
     public class Main
     {
-        
+        static string path;
+        static List<string> disks = new List<string>();
+
         public static void programm()
         {
             int[] positions_in_window;
             int position = 2;
-            positions_in_window = Wire.ShowDisks();
+            bool we_are_in_main = true;
+            Wire.ShowDisks(we_are_in_main, path);
             while (true)
             {
+                disks = Wire.Get_Path(we_are_in_main, path);
                 ConsoleKeyInfo key = Console.ReadKey();
+                Console.Clear();
                 if (key.Key == ConsoleKey.UpArrow)
                 {
+                    positions_in_window = Wire.ShowDisks(we_are_in_main, path);
                     Arrows arrow = new Arrows(positions_in_window[1] - 1, positions_in_window[0] - 1);
                     position--;
                     position = arrow.Up(arrow, position);
                 }
                 else if (key.Key == ConsoleKey.DownArrow)
                 {
+                    positions_in_window = Wire.ShowDisks(we_are_in_main, path);
                     Arrows arrow = new Arrows(positions_in_window[1], positions_in_window[0] - 1);
                     position++;
-                    position = arrow.Down(arrow, position);
+                    position = arrow.Down(arrow, position);    
                 }
                 else if (key.Key == ConsoleKey.Escape)
                 {
                     Console.Clear();
-                    Wire.ShowDisks();
+                    we_are_in_main = true;
+                    path = "";
+                    position = 2;
+                    Wire.ShowDisks(we_are_in_main, path);
                 }
                 else if (key.Key == ConsoleKey.Enter)
                 {
-
+                    if (we_are_in_main)
+                    {
+                        path += disks[position - 2];
+                        Console.Clear();
+                        we_are_in_main = false;
+                        position = 2;
+                        Wire.ShowDisks(we_are_in_main, path);
+                    }
+                    else
+                    {
+                        path = disks[position - 2];
+                        Console.Clear();
+                        position = 2;
+                        Wire.ShowDisks(we_are_in_main, path);
+                    }
                 }
                 else if (key.Key == ConsoleKey.F1)
                 {
--- Wire.cs
+++ Wire.cs
@@ -4,29 +4,112 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Wire;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Reflection.Metadata;
 
 namespace Wire
 {
     static class Wire
     {
-        public static int[] ShowDisks()
+        public static int[] ShowDisks(bool main_or_not, string path="C:\\")
         {
-            /*Console.SetCursorPosition(10, 0);*/
-            Console.WriteLine("This Computer");
-            Console.WriteLine("-----------------------------------------------");
-            int string_count = 2;
-            DriveInfo[] drives = DriveInfo.GetDrives();
-            int i = 2;
-            int available_pos = 0;
-            foreach (DriveInfo drive in drives)
+            if (main_or_not)
             {
-                Console.SetCursorPosition(2, i);
-                Console.WriteLine(drive.Name + " " + drive.AvailableFreeSpace / (1024 * 1024 * 1024) + " GB from " + drive.TotalSize / (1024 * 1024 *1024) + " GB");
-                i++;
-                available_pos++;
+                Console.WriteLine("TThis Computer");
+                Console.WriteLine("----------------------------------------------");
+                int string_count = 2;
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                int i = 2;
+                int available_pos = 0;
+
+                foreach (DriveInfo drive in drives)
+                {
+                    Console.SetCursorPosition(2, i);
+                    Console.WriteLine(drive.Name + " " + drive.AvailableFreeSpace / (1024 * 1024 * 1024) + " GB from " + drive.TotalSize / (1024 * 1024 * 1024) + " GB");
+                    i++;
+                    available_pos++;
+                }
+                int[] rows_in_window = { available_pos + string_count, string_count };
+                return rows_in_window;
             }
-            int[] rows_in_window = {available_pos + string_count, string_count};
-            return rows_in_window;
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("F1 - Create a directory, F2 - Create a file, F3 - Delete");
+                Console.WriteLine("----------------------------------------------------------");
+                string[] files = Directory.GetFiles(path);
+                string[] folders = Directory.GetDirectories(path);
+                
+                List<string> content = new List<string>();
+                int string_count = 2;
+                int available_pos = 0;
+                foreach (string folder in folders)
+                {
+                    content.Add(folder);
+                    available_pos++;
+                }
+                foreach (string file in files)
+                {
+                    content.Add(file);
+                    available_pos++;
+                }
+                int i = 2;
+                int max = 0;
+                foreach (string el in content)
+                {
+                    if (el.Length > max)
+                    {
+                        max = el.Length;
+                    }
+                }
+                foreach (string elem in content)
+                {
+                    Console.SetCursorPosition(2, i);
+                    Console.WriteLine(elem);
+                    Console.SetCursorPosition(max + 10, i);
+                    Console.WriteLine(Directory.GetCreationTime(elem));
+                    Console.SetCursorPosition((max + 10) + 30, i);
+                    Console.WriteLine(System.IO.Path.GetExtension(elem));
+                    i++;
+                }
+                
+                int[] rows = { available_pos + string_count, string_count };
+                return rows;
+            }
+            
         }
+
+        public static List<string> Get_Path(bool main_or_not, string path="C:\\")
+        {
+            if (main_or_not)
+            {
+                List<string> paths = new List<string>();
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (DriveInfo drive in drives)
+                {
+                    paths.Add(drive.Name);
+                }
+                return paths;
+            }
+            else
+            {
+                List<string> paths = new List<string>();
+                string[] files = Directory.GetFiles(path);
+                string[] folders = Directory.GetDirectories(path);
+
+                foreach (string folder in folders)
+                {
+                    paths.Add(folder);
+                }
+                foreach (string file in files)
+                {
+                    paths.Add(file);
+                }
+                return paths;
+            }
+        }
+
     }
 }
